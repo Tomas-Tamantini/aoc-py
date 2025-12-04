9eb9854bd167cc79fbaed56c71bc62edbdee486c
Tomas Barroso Fernandes Tamantini <tomasbft@gmail.com>
2025-12-04 15:14:24 -0300

    AOC 2025/04 - Solve part 2

--- src/solutions/y2025/d04/logic/paper_roll.py
+++ src/solutions/y2025/d04/logic/paper_roll.py
@@ -1,8 +1,30 @@
+from typing import Iterator
+
 from src.solutions.shared.geometry.vector_2d import Vector2D
 
 
+def _neighbors(
+    roll_pos: Vector2D, other_rolls: set[Vector2D]
+) -> Iterator[Vector2D]:
+    for n in roll_pos.neighbors(include_diagonals=True):
+        if n in other_rolls:
+            yield n
+
+
 def roll_is_reachable(roll_pos: Vector2D, other_rolls: set[Vector2D]) -> bool:
-    num_neighbors = sum(
-        n in other_rolls for n in roll_pos.neighbors(include_diagonals=True)
-    )
+    num_neighbors = len(list(_neighbors(roll_pos, other_rolls)))
     return num_neighbors < 4
+
+
+def removable_rolls(roll_positions: set[Vector2D]) -> set[Vector2D]:
+    remaining = roll_positions.copy()
+    rolls_to_remove = {
+        r for r in roll_positions if roll_is_reachable(r, roll_positions)
+    }
+    while rolls_to_remove:
+        roll = rolls_to_remove.pop()
+        remaining.remove(roll)
+        for neighbor in _neighbors(roll, remaining):
+            if roll_is_reachable(neighbor, remaining):
+                rolls_to_remove.add(neighbor)
+    return roll_positions - remaining
--- src/solutions/y2025/d04/solution.py
+++ src/solutions/y2025/d04/solution.py
@@ -1,6 +1,9 @@
 from src.core.io_handler import IOHandler
 from src.solutions.shared.parser.grid_parser import parse_grid
-from src.solutions.y2025.d04.logic.paper_roll import roll_is_reachable
+from src.solutions.y2025.d04.logic.paper_roll import (
+    removable_rolls,
+    roll_is_reachable,
+)
 
 
 def solve(io_handler: IOHandler) -> None:
@@ -12,6 +15,7 @@ def solve(io_handler: IOHandler) -> None:
     num_reachable_rolls = sum(
         roll_is_reachable(p, roll_positions) for p in roll_positions
     )
-
     io_handler.write_result(*prob_id, part=1, result=num_reachable_rolls)
-    io_handler.write_result(*prob_id, part=2, result="not implemented")
+
+    removed = len(removable_rolls(roll_positions))
+    io_handler.write_result(*prob_id, part=2, result=removed)

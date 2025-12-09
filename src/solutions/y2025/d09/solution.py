2c552bed314a1ac4af0557c5d1b380a91ef83d40
Tomas Barroso Fernandes Tamantini <tomasbft@gmail.com>
2025-12-09 07:42:05 -0300

    AOC 2025/09 - Solve part 1

--- src/solutions/y2025/d09/solution.py
+++ src/solutions/y2025/d09/solution.py
@@ -1,8 +1,28 @@
+from itertools import combinations
+
 from src.core.io_handler import IOHandler
+from src.solutions.shared.geometry import Vector2D
+from src.solutions.shared.parser import parse_csv
+
+
+def _rectangle_area(corner_a: Vector2D, corner_b: Vector2D) -> int:
+    diff = corner_a - corner_b
+    return (abs(diff.x) + 1) * (abs(diff.y) + 1)
 
 
 def solve(io_handler: IOHandler) -> None:
     prob_id = 2025, 9
 
-    io_handler.write_result(*prob_id, part=1, result="not implemented")
+    positions = list(
+        parse_csv(
+            io_handler.input_reader(*prob_id),
+            mapper=lambda values: Vector2D(*map(int, values)),
+        )
+    )
+
+    max_area = max(
+        _rectangle_area(*corners) for corners in combinations(positions, 2)
+    )
+
+    io_handler.write_result(*prob_id, part=1, result=max_area)
     io_handler.write_result(*prob_id, part=2, result="not implemented")

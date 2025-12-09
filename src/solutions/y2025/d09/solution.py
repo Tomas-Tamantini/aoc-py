380353ae253fc771acbaf20ca4e1686cedabada2
Tomas Barroso Fernandes Tamantini <tomasbft@gmail.com>
2025-12-09 07:37:36 -0300

    AOC 2025/09 - Setup solution directory

---
+++ src/solutions/y2025/d09/solution.py
@@ -0,0 +1,8 @@
+from src.core.io_handler import IOHandler
+
+
+def solve(io_handler: IOHandler) -> None:
+    prob_id = 2025, 9
+
+    io_handler.write_result(*prob_id, part=1, result="not implemented")
+    io_handler.write_result(*prob_id, part=2, result="not implemented")

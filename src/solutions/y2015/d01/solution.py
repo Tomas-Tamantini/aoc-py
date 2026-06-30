6bdb608d84a701ed863f548ebf9481a25b34c4c1
Tomas Tamantini <tomasbft@gmail.com>
2026-06-30 15:58:47 -0300

    AOC 2015/01 - Solve

--- src/solutions/y2015/d01/solution.py
+++ src/solutions/y2015/d01/solution.py
@@ -1,8 +1,22 @@
+from typing import Iterator
+
 from src.core.io_handler import IOHandler
+
+
+def _floors(instructions: str) -> Iterator[int]:
+    floor = 0
+    yield floor
+    for c in instructions:
+        offset = 1 if c == "(" else -1
+        floor += offset
+        yield floor
 
 
 def solve(io_handler: IOHandler) -> None:
     prob_id = 2015, 1
+    instructions = io_handler.input_reader(*prob_id).read_input().strip()
 
-    io_handler.write_result(*prob_id, part=1, result="not implemented")
-    io_handler.write_result(*prob_id, part=2, result="not implemented")
+    floors = list(_floors(instructions))
+
+    io_handler.write_result(*prob_id, part=1, result=floors[-1])
+    io_handler.write_result(*prob_id, part=2, result=floors.index(-1))

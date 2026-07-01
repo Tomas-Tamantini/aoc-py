ab66863d3f55eaaa233c9e28dd4c853b9cf4f995
Tomas Tamantini <tomasbft@gmail.com>
2026-07-01 06:58:15 -0300

    AOC 2015/02 - Use csv parser

--- src/solutions/y2015/d02/logic/parser.py
+++ src/solutions/y2015/d02/logic/parser.py
@@ -1,15 +1,15 @@
 from typing import Iterator
 
 from src.core.input_reader import InputReader
+from src.solutions.shared.parser.csv_parser import parse_csv
 from src.solutions.y2015.d02.logic.rectangular_box import RectangularBox
-
-
-def _parse_rectangular_box(line: str) -> RectangularBox:
-    return RectangularBox(*map(int, line.split("x")))
 
 
 def parse_rectangular_boxes(
     input_reader: InputReader,
 ) -> Iterator[RectangularBox]:
-    for line in input_reader.read_stripped_lines():
-        yield _parse_rectangular_box(line)
+    yield from parse_csv(
+        input_reader,
+        separator="x",
+        mapper=lambda values: RectangularBox(*map(int, values)),
+    )

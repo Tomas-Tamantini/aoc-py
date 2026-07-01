117c1c3fabd4f63190c01d06d233c064139a36fb
Tomas Tamantini <tomasbft@gmail.com>
2026-07-01 07:28:02 -0300

    AOC 2015 /03 - Solve

--- src/solutions/y2015/d03/solution.py
+++ src/solutions/y2015/d03/solution.py
@@ -1,8 +1,39 @@
+from typing import Iterable, Iterator
+
 from src.core.io_handler import IOHandler
+from src.solutions.shared.geometry import Direction, Vector2D
+
+
+def _parse_instructions(input_reader) -> Iterator[Direction]:
+    directions = {
+        "^": Direction.UP,
+        "v": Direction.DOWN,
+        "<": Direction.LEFT,
+        ">": Direction.RIGHT,
+    }
+    for c in input_reader.read_input():
+        yield directions[c]
+
+
+def _visited_houses(instructions: Iterable[Direction]) -> Iterator[Vector2D]:
+    position = Vector2D(0, 0)
+    yield position
+    for direction in instructions:
+        position = position.move(direction)
+        yield position
 
 
 def solve(io_handler: IOHandler) -> None:
     prob_id = 2015, 3
 
-    io_handler.write_result(*prob_id, part=1, result="not implemented")
-    io_handler.write_result(*prob_id, part=2, result="not implemented")
+    instructions = list(_parse_instructions(io_handler.input_reader(*prob_id)))
+
+    visited_p1 = set(_visited_houses(instructions))
+    io_handler.write_result(*prob_id, part=1, result=len(visited_p1))
+
+    instructions_santa = instructions[::2]
+    instructions_robo = instructions[1::2]
+    visited_santa = set(_visited_houses(instructions_santa))
+    visited_robo = set(_visited_houses(instructions_robo))
+    visited_p2 = visited_santa.union(visited_robo)
+    io_handler.write_result(*prob_id, part=2, result=len(visited_p2))
